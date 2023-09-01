b65dd7b8d4105e931c30a114fecf67ce5a15ecda
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-09-01 07:23:04 +0100

    added services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FC } from 'react';
 import Image from 'next/image';
 
 import { Button } from '@/components/ui';
@@ -5,26 +6,33 @@ import HeroAnimation from '@/components/HeroAnimation/HeroAnimation';
 
 export default function Home() {
     return (
-        <main className="flex">
-            <section id="hero" className="flex grow">
-                <div>
-                    <div className="container min-h-screen grow flex items-center sm:justify-between flex-col md:flex-row pt-28 md:pt-0 gap-10">
-                        <HeroContent />
-                        <HeroAnimation />
-                    </div>
-                    <div>
-                        <Image
-                            width={1483.87}
-                            height={163.42}
-                            className="w-screen"
-                            alt="section-underline"
-                            id="hero-section-underline"
-                            src="/hero-section-underline.svg"
-                        />
-                    </div>
-                </div>
-            </section>
+        <main className="flex flex-col">
+            <Hero />
+            <Services />
         </main>
+    );
+}
+
+function Hero() {
+    return (
+        <section id="hero" className="flex grow">
+            <div>
+                <div className="container min-h-screen grow flex items-center sm:justify-between flex-col md:flex-row pt-28 md:pt-0 gap-10">
+                    <HeroContent />
+                    <HeroAnimation />
+                </div>
+                <div>
+                    <Image
+                        width={1483.87}
+                        height={163.42}
+                        className="w-screen"
+                        alt="section-underline"
+                        id="hero-section-underline"
+                        src="/hero-section-underline.svg"
+                    />
+                </div>
+            </div>
+        </section>
     );
 }
 
@@ -47,3 +55,62 @@ function HeroContent() {
         </div>
     );
 }
+
+interface ServiceProps {
+    imagePath: string;
+    description: string;
+}
+
+const servicesContent: ServiceProps[] = [
+    {
+        imagePath: '/airport-rafiki.svg',
+        description:
+            "You couldn't find anyone willing to join you on your adventurous trip.",
+    },
+    {
+        imagePath: '/trip-pana.svg',
+        description:
+            'Sometimes you wished you had a travel buddy to share a few moments with, even if you love traveling solo.',
+    },
+    {
+        imagePath: '/online-transactions-pana.svg',
+        description:
+            'Having someone to split the costs with would have made your life easier',
+    },
+];
+
+function Services() {
+    return (
+        <section id="services" className="pt-16 pb-9">
+            <div className="container flex flex-col gap-16">
+                <h2 className="opacity-80 text-center text-dark-blue text-2xl sm:text-3xl md:text-4xl lg:text-[44px] font-medium leading-tight">
+                    Have you ever felt this way?
+                </h2>
+                <div className="flex items-stretch justify-center gap-6 flex-wrap lg:flex-nowrap">
+                    {servicesContent.map((serviceContent) => (
+                        <Service
+                            {...serviceContent}
+                            key={serviceContent.imagePath}
+                        />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+const Service: FC<ServiceProps> = ({ imagePath, description }) => {
+    return (
+        <article className="grow w-full px-4 sm:px-6 md:px-9 py-4 sm:py-6 md:py-8 bg-white rounded-[20px] border border-neutral-200 justify-center items-center flex-col inline-flex">
+            <Image
+                width={300}
+                height={243}
+                src={imagePath}
+                alt="service artwork"
+            />
+            <p className="text-center text-zinc-600 text-base sm:text-xl font-normal leading-[29.29px]">
+                {description}
+            </p>
+        </article>
+    );
+};
