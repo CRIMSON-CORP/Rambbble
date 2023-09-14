3e15a62e8937c139b445b7bd662e11ea315619b3
Oluwatowo Rosanwo <olueatoworosanwo@gmail.com>
2023-09-14 20:09:42 +0100

    setup applitools

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from "cypress";
+import eyesPlugin from '@applitools/eyes-cypress';
+import { defineConfig } from 'cypress';
 
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
+export default eyesPlugin(
+    defineConfig({
+        e2e: {
+            setupNodeEvents(on, config) {
+                // implement node event listeners here
+            },
+        },
 
-  viewportWidth: 1440,
-  viewportHeight: 900,
+        viewportWidth: 1440,
+        viewportHeight: 900,
 
-  component: {
-    devServer: {
-      framework: "next",
-      bundler: "webpack",
-    },
-  },
-});
+        component: {
+            devServer: {
+                framework: 'next',
+                bundler: 'webpack',
+            },
+        },
+    }),
+);
--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -30,4 +30,41 @@ describe('render navbar', () => {
             cy.location('pathname').should('equal', link.path);
         });
     });
+describe('homepage ui is proper', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000');
+        // Open Eyes to start visual testing.
+        // Each test should open its own Eyes for its own snapshots.
+        cy.eyesOpen({
+            // The name of the application under test.
+            // All tests for the same app should share the same app name.
+            // Set this name wisely: Applitools features rely on a shared app name across tests.
+            appName: 'Rambbble',
+            // The name of the test case for the given application.
+            // Additional unique characteristics of the test may also be specified as part of the test name,
+            // such as localization information ("Home Page - EN") or different user permissions ("Login by admin").
+            testName: Cypress.currentTest.title,
+        });
+    });
+
+    it('renders homepage correctly', () => {
+        cy.wait(5000);
+        cy.get('footer').scrollIntoView({
+            duration: 3800,
+        });
+
+        cy.wait(10000);
+        cy.eyesCheckWindow({
+            tag: 'Homepage',
+            target: 'window',
+            fully: true,
+        });
+    });
+
+    afterEach(() => {
+        // Close Eyes to tell the server it should display the results.
+        cy.eyesClose();
+    });
+});
+
 });
